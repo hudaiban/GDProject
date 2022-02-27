e8acb14de78cfa3a17abf13ed3447ed14a8feea0
hudaiban <hudaiban01@hotmail.com>
2022-02-27 19:15:34 +0300

    first update

--- controller/routeController.js
+++ controller/routeController.js
@@ -1,36 +1,32 @@
-
-module.exports =
-{
-    
-    home: (req, res) => {
-        res.render('index.ejs');
-    },
-    gpa: (req, res) => {
-
-        res.render('gpa.ejs');
-
-    },
-    planner: (req, res) => {
-
-        res.render('planner.ejs')
-    },
-    vote: (req, res) => {
-
-        res.render('vote.ejs');
-
-    },
-    login: (req, res) => {
-
-        res.render('login.ejs')
-    },
-     relogin: (req, res) => {
-
-        res.render('relogin.ejs')
-    },
-    register: (req, res) => {
-
-        res.render('register.ejs')
-    },
-
-
-}
+module.exports = {
+  home: (req, res) => {
+    res.render("index.ejs");
+  },
+  gpa: (req, res) => {
+    res.render("gpa.ejs");
+  },
+  planner: (req, res) => {
+    res.render("planner.ejs");
+  },
+  vote: (req, res) => {
+    res.render("vote.ejs");
+  },
+  login: (req, res) => {
+    res.render("login.ejs");
+  },
+  relogin: (req, res) => {
+    res.render("relogin.ejs");
+  },
+  register: (req, res) => {
+    res.render("register.ejs");
+  },
+  dashbaord: (req, res) => {
+    res.render("dashboard.ejs");
+  },
+  profile: (req, res) => {
+    res.render("profile.ejs");
+  },
+  about: (req, res) => {
+    res.render("about.ejs");
+  },
+};
--- routes/route.js
+++ routes/route.js
@@ -1,31 +1,33 @@
-const router = require('express').Router();
-const routecontroller = require('../controller/routeController');
-const auth = require('../controller/authController');
-const { route } = require('express/lib/router');
-
+const router = require("express").Router();
+const routecontroller = require("../controller/routeController");
+const auth = require("../controller/authController");
+const { route } = require("express/lib/router");
 
 // -- sign up -- //
 
-router.post('/register', auth.signup);
+router.post("/register", auth.signup);
 // -- Login -- //
-router.post('/login', auth.login);
+router.post("/login", auth.login);
 
-
-
-// home page 
-router.get('/', routecontroller.home);
+// home page
+router.get("/", routecontroller.home);
 // GPA page
-router.get('/gpa', routecontroller.gpa);
-// planner page 
-router.get('/planner', routecontroller.planner)
-// Vote page 
-router.get('/vote', routecontroller.vote);
+router.get("/gpa", routecontroller.gpa);
+// planner page
+router.get("/planner", routecontroller.planner);
+// Vote page
+router.get("/vote", routecontroller.vote);
 // login page
-router.get('/login', routecontroller.login);
-// register page 
-router.get('/register', routecontroller.register);
-// relogin 
-router.get('/relogin', routecontroller.relogin)
-
+router.get("/login", routecontroller.login);
+// register page
+router.get("/register", routecontroller.register);
+// relogin
+router.get("/relogin", routecontroller.relogin);
+// Home Page
+router.get("/dashboard", routecontroller.dashbaord);
+// Profile Page
+router.get("/profile", routecontroller.profile);
+// About Page
+router.get("/about", routecontroller.about);
 
 module.exports = router;
